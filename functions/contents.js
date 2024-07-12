f15315988a7fa57bb0e495cd67b03c8d215807e9
Oksana Mironova <kq3hip13z@gmail.com>
2024-07-12 20:52:51 +0200

    cors

--- functions/contents.js
+++ functions/contents.js
@@ -4,6 +4,10 @@ const contents = cards;
 exports.handler = async function (event, context) {
 	return {
 		statusCode: 200,
+		headers: {
+			'Access-Control-Allow-Origin': '*', // Allow all origins
+			'Access-Control-Allow-Headers': 'Content-Type' // Allow specific headers
+		},
 		body: JSON.stringify(contents, null, 2)
 	};
 };
